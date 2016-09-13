45c1c8bb82a9a6fe63ce93f35e5bbb5778861934
Tim Holy <tim.holy@gmail.com>
2016-09-13 05:10:05 -0500

    Check inferrability of more operations

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,20 +38,20 @@ end
     @testset "> Unitless ↔ unitful conversion" begin
         @test_throws MethodError convert(typeof(3m),1)
         @test_throws ErrorException convert(Float64, 3m)
-        @test 3m/unit(3m) === 3
-        @test 3.0g/unit(3.0g) === 3.0
+        @test @inferred(3m/unit(3m)) === 3
+        @test @inferred(3.0g/unit(3.0g)) === 3.0
     end
 
     @testset "> Unitful ↔ unitful conversion" begin
         @testset ">> Numeric conversion" begin
-            @test float(3m) === 3.0m
-            @test Integer(3.0A) === 3A
+            @test @inferred(float(3m)) === 3.0m
+            @test @inferred(Integer(3.0A)) === 3A
             @test Rational(3.0m) === (3//1)*m
         end
         @testset ">> Intra-unit conversion" begin
-            @test uconvert(g,1g) == 1g
+            @test @inferred(uconvert(g,1g)) == 1g
             # an essentially no-op uconvert should not disturb numeric type
-            @test uconvert(m,0x01*m) == 0x01*m
+            @test @inferred(uconvert(m,0x01*m)) == 0x01*m
             # test special case of temperature
             @test uconvert(°C, 0x01*°C) == 0x01*°C
             @test 1kg === 1kg
@@ -67,7 +67,7 @@ end
         @testset ">> Temperature conversion" begin
             # When converting a pure temperature, offsets in temperature are
             # taken into account. If you like °Ra seek help
-            @test uconvert(°Ra, 4.2K) ≈ 7.56°Ra
+            @test @inferred(uconvert(°Ra, 4.2K)) ≈ 7.56°Ra
             @test uconvert(°F, 0°C) == 32°F
             @test uconvert(°C, 212°F) == 100°C
 
@@ -81,11 +81,11 @@ end
 
 @testset "Promotion" begin
     @testset "> Simple promotion" begin
-        @test promote(1.0m, 1m) == (1.0m, 1.0m)
-        @test promote(1m, 1.0m) == (1.0m, 1.0m)
-        @test promote(1.0m, 1kg) == (1.0m, 1.0kg)
-        @test promote(1kg, 1.0m) == (1.0kg, 1.0m)
-        @test promote(1.0m, 1) == (1.0m, UnitlessQuantity(1.0))
+        @test @inferred(promote(1.0m, 1m)) == (1.0m, 1.0m)
+        @test @inferred(promote(1m, 1.0m)) == (1.0m, 1.0m)
+        @test @inferred(promote(1.0m, 1kg)) == (1.0m, 1.0kg)
+        @test @inferred(promote(1kg, 1.0m)) == (1.0kg, 1.0m)
+        @test @inferred(promote(1.0m, 1)) == (1.0m, UnitlessQuantity(1.0))
     end
 
     @testset "> Promotion during array creation" begin
@@ -100,15 +100,15 @@ end
 end
 
 @testset "Unit and dimensional analysis" begin
-    @test unit(1m^2) == m^2
-    @test unit(typeof(1m^2)) == m^2
-    @test dimension(1m^2) == 𝐋^2
-    @test dimension(typeof(1m^2)) == 𝐋^2
-    @test dimension(m^2) == 𝐋^2
-    @test dimension(1m/s) == 𝐋/𝐓
-    @test dimension(m/s) == 𝐋/𝐓
-    @test dimension(1u"mol") == 𝐍
-    @test dimension(μm/m) == Unitful.Dimensions{()}()
+    @test @inferred(unit(1m^2)) == m^2
+    @test @inferred(unit(typeof(1m^2))) == m^2
+    @test @inferred(dimension(1m^2)) == 𝐋^2
+    @test @inferred(dimension(typeof(1m^2))) == 𝐋^2
+    @test @inferred(dimension(m^2)) == 𝐋^2
+    @test @inferred(dimension(1m/s)) == 𝐋/𝐓
+    @test @inferred(dimension(m/s)) == 𝐋/𝐓
+    @test @inferred(dimension(1u"mol")) == 𝐍
+    @test @inferred(dimension(μm/m)) == Unitful.Dimensions{()}()
     @test dimension([1u"m", 1u"s"]) == [𝐋, 𝐓]
     @test (𝐋/𝐓)^2 == 𝐋^2 / 𝐓^2
     @test isa(m, LengthUnit)
@@ -153,23 +153,23 @@ end
     end
 
     @testset "> Addition and subtraction" begin
-        @test +(1A) == 1A                     # Unary addition
-        @test 3m + 3m == 6m                   # Binary addition
-        @test -(1kg) == (-1)*kg               # Unary subtraction
-        @test 3m - 2m == 1m                   # Binary subtraction
-        @test zero(1m) === 0m                 # Additive identity
-        @test zero(typeof(1m)) === 0m
-        @test zero(typeof(1.0m)) === 0.0m
+        @test @inferred(+(1A)) == 1A                     # Unary addition
+        @test @inferred(3m + 3m) == 6m                   # Binary addition
+        @test @inferred(-(1kg)) == (-1)*kg               # Unary subtraction
+        @test @inferred(3m - 2m) == 1m                   # Binary subtraction
+        @test @inferred(zero(1m)) === 0m                 # Additive identity
+        @test @inferred(zero(typeof(1m))) === 0m
+        @test @inferred(zero(typeof(1.0m))) === 0.0m
         @test_throws ErrorException 1+1m
         @test_throws ErrorException 1-1m
     end
 
     @testset "> Multiplication" begin
-        @test *(1s) == 1s                     # Unary multiplication
-        @test 3m * 2cm == 3cm * 2m            # Binary multiplication
-        @test (3m)*m == 3*(m*m)               # Associative multiplication
-        @test true*1kg == 1kg                 # Boolean multiplication (T)
-        @test false*1kg == 0kg                # Boolean multiplication (F)
+        @test @inferred(*(1s)) == 1s                     # Unary multiplication
+        @test @inferred(3m * 2cm) == 3cm * 2m            # Binary multiplication
+        @test @inferred((3m)*m) == 3*(m*m)               # Associative multiplication
+        @test @inferred(true*1kg) == 1kg                 # Boolean multiplication (T)
+        @test @inferred(false*1kg) == 0kg                # Boolean multiplication (F)
     end
 
     @testset "> Division" begin
@@ -189,19 +189,19 @@ end
     end
 
     @testset "> Exponentiation" begin
-        @test m^3/m == m^2
-        @test 𝐋^3/𝐋 == 𝐋^2
-        @test sqrt(4m^2) == 2m                # sqrt works
+        @test @inferred(m^3/m) == m^2
+        @test @inferred(𝐋^3/𝐋) == 𝐋^2
+        @test @inferred(sqrt(4m^2)) == 2m                # sqrt works
         @test sqrt(4m^(2//3)) == 2m^(1//3)    # less trivial example
-        @test sqrt(𝐋^2) == 𝐋
-        @test sqrt(m^2) == m
+        @test @inferred(sqrt(𝐋^2)) == 𝐋
+        @test @inferred(sqrt(m^2)) == m
         @test (2m)^3 == 8*m^3
         @test (8m)^(1//3) == 2*m^(1//3)
     end
 
     @testset "> Trigonometry" begin
-        @test sin(90°) == 1                   # sin(degrees) works
-        @test cos(π*rad) == -1                # ...radians work
+        @test @inferred(sin(90°)) == 1                   # sin(degrees) works
+        @test @inferred(cos(π*rad)) == -1                # ...radians work
     end
 
     @testset "> Is functions" begin
@@ -219,29 +219,29 @@ end
 end
 
 @testset "Rounding" begin
-    @test trunc(3.7m) == 3.0m
+    @test @inferred(trunc(3.7m)) == 3.0m
     @test trunc(-3.7m) == -3.0m
-    @test floor(3.7m) == 3.0m
+    @test @inferred(floor(3.7m)) == 3.0m
     @test floor(-3.7m) == -4.0m
-    @test ceil(3.7m) == 4.0m
+    @test @inferred(ceil(3.7m)) == 4.0m
     @test ceil(-3.7m) == -3.0m
-    @test round(3.7m) == 4.0m
+    @test @inferred(round(3.7m)) == 4.0m
     @test round(-3.7m) == -4.0m
 end
 
 @testset "Sgn, abs, &c." begin
-    @test abs(3V+4V*im) == 5V
-    @test norm(3V+4V*im) == 5V
-    @test abs2(3V+4V*im) == 25V^2
-    @test abs(-3m) == 3m
-    @test abs2(-3m) == 9m^2
-    @test sign(-3.3m) == -1.0
-    @test signbit(0.0m) == false
-    @test signbit(-0.0m) == true
-    @test copysign(3.0m, -4.0s) == -3.0m
-    @test copysign(3.0m, 4) == 3.0m
-    @test flipsign(3.0m, -4) == -3.0m
-    @test flipsign(-3.0m, -4) == 3.0m
+    @test @inferred(abs(3V+4V*im)) == 5V
+    @test norm(3V+4V*im) == 5V  # FIXME: add @inferred
+    @test @inferred(abs2(3V+4V*im)) == 25V^2
+    @test @inferred(abs(-3m)) == 3m
+    @test @inferred(abs2(-3m)) == 9m^2
+    @test @inferred(sign(-3.3m)) == -1.0
+    @test @inferred(signbit(0.0m)) == false
+    @test @inferred(signbit(-0.0m)) == true
+    @test @inferred(copysign(3.0m, -4.0s)) == -3.0m
+    @test @inferred(copysign(3.0m, 4)) == 3.0m
+    @test @inferred(flipsign(3.0m, -4)) == -3.0m
+    @test @inferred(flipsign(-3.0m, -4)) == 3.0m
 end
 
 @testset "Collections" begin
@@ -249,12 +249,12 @@ end
     @testset "> Ranges" begin
 
         @testset ">> Some of test/ranges.jl, with units" begin
-            @test size(10m:1m:0m) == (0,)
+            @test @inferred(size(10m:1m:0m)) == (0,)
             # @test length(1m:.2m:2m) == 6
             # @test length(1.0m:.2m:2.0m) == 6
             # @test length(2m:-.2m:1m) == 6
             # @test length(2.0m:-.2m:1.0m) == 6
-            @test length(2m:.2m:1m) == 0
+            @test @inferred(length(2m:.2m:1m)) == 0
             @test length(2.0m:.2m:1.0m) == 0
 
             @test length(1m:2m:0m) == 0
@@ -266,7 +266,7 @@ end
     #         @test L32[4] == 4m && L64[4] == 4m
 
             r = 5m:-1m:1m
-            @test r[1]==5m
+            @test @inferred(r[1])==5m
             @test r[2]==4m
             @test r[3]==3m
             @test r[4]==2m
@@ -274,7 +274,7 @@ end
 
             # @test length(.1m:.1m:.3m) == 3
             # @test length(1.1m:1.1m:3.3m) == 3
-            @test length(1.1m:1.3m:3m) == 2
+            @test @inferred(length(1.1m:1.3m:3m)) == 2
             @test length(1m:1m:1.8m) == 1
 
             @test (1m:2m:13m)[2:6] == 3m:2m:11m
@@ -284,16 +284,16 @@ end
         end
 
         @testset ">> StepRange" begin
-            r = 1m:1m:5m
+            r = @inferred(colon(1m, 1m, 5m)) # 1m:1m:5m
             @test isa(r, StepRange)
-            @test length(r) === 5
-            @test step(r) === 1m
+            @test @inferred(length(r)) === 5
+            @test @inferred(step(r)) === 1m
         end
 
         @testset ">> Float StepRange" begin
-            @test isa((1.0m:1m:5m), StepRange{typeof(1.0m)})
-            @test length(1.0m:1m:5m) === 5
-            @test step(1.0m:1m:5m) === 1.0m
+            @test isa(@inferred(colon(1.0m, 1m, 5m)), StepRange{typeof(1.0m)})
+            @test @inferred(length(1.0m:1m:5m)) === 5
+            @test @inferred(step(1.0m:1m:5m)) === 1.0m
 
             @test_throws ArgumentError 1.0m:0.0m:5.0m
         end
