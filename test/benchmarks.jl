2bda342f92e751d3250b9f462bd2ab0eebf3b8a4
Mayeul d'Avezac <mdavezac@gmail.com>
2016-10-04 20:55:08 +0100

    Adding default benchmarks

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -23,17 +23,33 @@ benchmark(expression::Expr) =
     benchmark!(BenchmarkGroup(["Unitful"]), expression)
 
 function performance_summary(suite)
+    tune!(suite)
     benchs = run(suite)
-    result = DataFrame(op=Symbol[], expression=String[], performance=Float64[])
+    result = DataFrame(op=Symbol[], expression=String[],
+                       ratio=Float64[], time=Any[], memory=Any[])
     for op in keys(benchs)
         for expression in keys(benchs[op])
-            units = benchs[op][expression][:units]
-            nounits = benchs[op][expression][:nounits]
+            units = median(benchs[op][expression][:units])
+            nounits = median(benchs[op][expression][:nounits])
+            rt = ratio(units, nounits).time
+            judgment = judge(units, nounits)
             push!(
                 result,
-                [op, expression, ratio(median(units), median(nounits)).time]
+                [op, expression, rt, judgment.time, judgment.memory]
             )
         end
     end
     result
 end
+
+function benchmark()
+    suite = BenchmarkGroup(["Unitful"])
+    for a in (2u"m", 2.1u"m"), b in (3u"kg", 3.5u"kg"), op in (:*, :/)
+      benchmark!(suite, :($op($a, $b)))
+    end
+    # for a in (2u"m", 1//2u"m"), b in (1//3u"kg"), op in (:*, :+, :-, ://)
+    #     benchmark!(suite, :($op($a, $b)))
+    # end
+    performance_summary(suite)
+end
+
