a477f2154f3af097913324a4b22a429cf3947414
Andrew Keller <andrew.keller.09@gmail.com>
2016-10-11 15:56:58 -0700

    Implemented most basic fastmath ops

--- src/fastmath.jl
+++ src/fastmath.jl
@@ -1,6 +1,8 @@
 import Base.FastMath
-import Core.Intrinsics
+import Core.Intrinsics: box, unbox, powi_llvm, sqrt_llvm_fast
 import Base.FastMath: @fastmath,
+    FloatTypes,
+    ComplexTypes,
     add_fast,
     sub_fast,
     mul_fast,
@@ -14,54 +16,127 @@ import Base.FastMath: @fastmath,
     le_fast,
     fast_op
 
-FloatTypes = Union{Quantity{Float32}, Quantity{Float64}}
+sub_fast{T<:FloatTypes}(x::Quantity{T}) = typeof(x)(box(T,
+    Base.neg_float_fast(unbox(T,x.val))))
 
-sub_fast{T<:FloatTypes}(x::T) = box(T,Base.neg_float_fast(unbox(T,x)))
+add_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Quantity{T,D,U}(box(T,
+        Base.add_float_fast(unbox(T,x.val), unbox(T,y.val))))
 
-add_fast{T<:FloatTypes}(x::T, y::T) =
-    box(T,Base.add_float_fast(unbox(T,x), unbox(T,y)))
-sub_fast{T<:FloatTypes}(x::T, y::T) =
-    box(T,Base.sub_float_fast(unbox(T,x), unbox(T,y)))
-@generated function mul_fast{T<:FloatTypes}(x::T, y::T)
-    S = typeof(T(1)*T(1))
-    :(box($S,Base.mul_float_fast(unbox(T,x), unbox(T,y))))
+sub_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Quantity{T,D,U}(box(T,
+        Base.sub_float_fast(unbox(T,x.val), unbox(T,y.val))))
+
+function mul_fast{T<:FloatTypes}(x::Quantity{T}, y::Quantity{T})
+    D = typeof(dimension(x) * dimension(y))
+    U = typeof(unit(x) * unit(y))
+    Quantity{T,D,U}(box(T, Base.mul_float_fast(unbox(T,x.val), unbox(T,y.val))))
 end
-@generated function div_fast{T<:FloatTypes}(x::T, y::T)
-    S = typeof(div(T(1),T(1)))
-    :(box(S,Base.div_float_fast(unbox(T,x), unbox(T,y))))
+function div_fast{T<:FloatTypes}(x::Quantity{T}, y::Quantity{T})
+    D = typeof(dimension(x) / dimension(y))
+    U = typeof(unit(x) / unit(y))
+    Quantity{T,D,U}(box(T, Base.div_float_fast(unbox(T,x.val), unbox(T,y.val))))
 end
-rem_fast{T<:FloatTypes}(x::T, y::T) =
-    box(T,Base.rem_float_fast(unbox(T,x), unbox(T,y)))
 
-add_fast{T<:FloatTypes}(x::T, y::T, zs::T...) =
+rem_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Quantity{T,D,U}(box(T, Base.rem_float_fast(unbox(T,x.val), unbox(T,y.val))))
+
+add_fast{T<:FloatTypes}(x::Quantity{T}, y::Quantity{T}, zs::Quantity{T}...) =
     add_fast(add_fast(x, y), zs...)
-mul_fast{T<:FloatTypes}(x::T, y::T, zs::T...) =
+mul_fast{T<:FloatTypes}(x::Quantity{T}, y::Quantity{T}, zs::Quantity{T}...) =
     mul_fast(mul_fast(x, y), zs...)
 
 @fastmath begin
-    cmp_fast{T<:FloatTypes}(x::T, y::T) = ifelse(x==y, 0, ifelse(x<y, -1, +1))
-    function mod_fast{T<:FloatTypes}(x::T, y::T)
+    cmp_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+        ifelse(x==y, 0, ifelse(x<y, -1, +1))
+    function mod_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U})
         r = rem(x,y)
-        ifelse((r > zero(T)) $ (y > zero(T)), r+y, r)
+        ifelse((r > zero(Quantity{T,D,U})) $ (y > zero(Quantity{T,D,U})), r+y, r)
     end
 end
 
-eq_fast{T<:FloatTypes}(x::T, y::T) =
-    Base.eq_float_fast(unbox(T,x),unbox(T,y))
-ne_fast{T<:FloatTypes}(x::T, y::T) =
-    Base.ne_float_fast(unbox(T,x),unbox(T,y))
-lt_fast{T<:FloatTypes}(x::T, y::T) =
-    Base.lt_float_fast(unbox(T,x),unbox(T,y))
-le_fast{T<:FloatTypes}(x::T, y::T) =
-    Base.le_float_fast(unbox(T,x),unbox(T,y))
+eq_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Base.eq_float_fast(unbox(T,x.val),unbox(T,y.val))
+ne_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Base.ne_float_fast(unbox(T,x.val),unbox(T,y.val))
+lt_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Base.lt_float_fast(unbox(T,x.val),unbox(T,y.val))
+le_fast{T<:FloatTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+    Base.le_float_fast(unbox(T,x.val),unbox(T,y.val))
+
+@fastmath begin
+    abs_fast{T<:ComplexTypes}(x::Quantity{T}) = hypot(real(x), imag(x))
+    abs2_fast{T<:ComplexTypes}(x::Quantity{T}) = real(x)*real(x) + imag(x)*imag(x)
+    conj_fast{T<:ComplexTypes,D,U}(x::Quantity{T,D,U}) =
+        Quantity{T,D,U}(T(real(x.val), -imag(x.val)))
+    inv_fast{T<:ComplexTypes,D,U}(x::Quantity{T,D,U}) = conj(x) / abs2(x)
+    # sign_fast{T<:ComplexTypes}(x::T) = x == 0 ? float(zero(x)) : x/abs(x) #TODO
+
+    add_fast{T<:ComplexTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+        Quantity{T,D,U}(T(real(x.val)+real(y.val), imag(x.val)+imag(y.val)))
+    add_fast{T<:FloatTypes,D,U}(x::Quantity{Complex{T},D,U}, b::Quantity{T,D,U}) =
+        Quantity{T,D,U}(Complex{T}(real(x.val)+b.val, imag(x.val)))
+    add_fast{T<:FloatTypes,D,U}(a::Quantity{T,D,U}, y::Quantity{Complex{T},D,U}) =
+        Quantity{T,D,U}(Complex{T}(a.val+real(y.val), imag(y.val)))
+
+    sub_fast{T<:ComplexTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) =
+        Quantity{T,D,U}(T(real(x.val)-real(y.val), imag(x.val)-imag(y.val)))
+    sub_fast{T<:FloatTypes,D,U}(x::Quantity{Complex{T},D,U}, b::Quantity{T,D,U}) =
+        Quantity{T,D,U}(Complex{T}(real(x.val)-b.val, imag(x.val)))
+    sub_fast{T<:FloatTypes,D,U}(a::Quantity{T,D,U}, y::Quantity{Complex{T},D,U}) =
+        Quantity{T,D,U}(Complex{T}(a.val-real(y.val), -imag(y.val)))
+
+    function mul_fast{T<:ComplexTypes}(x::Quantity{T}, y::Quantity{T})
+        D = typeof(dimension(x) * dimension(y))
+        U = typeof(unit(x) * unit(y))
+        Quantity{T,D,U}(T(real(x.val)*real(y.val) - imag(x.val)*imag(y.val),
+          real(x.val)*imag(y.val) + imag(x.val)*real(y.val)))
+    end
+    function mul_fast{T<:FloatTypes}(x::Quantity{Complex{T}}, b::Quantity{T})
+        D = typeof(dimension(x) * dimension(y))
+        U = typeof(unit(x) * unit(y))
+        Quantity{T,D,U}(Complex{T}(real(x.val)*b.val, imag(x.val)*b.val))
+    end
+    function mul_fast{T<:FloatTypes}(a::Quantity{T}, y::Quantity{Complex{T}})
+        D = typeof(dimension(x) * dimension(y))
+        U = typeof(unit(x) * unit(y))
+        Quantity{T,D,U}(Complex{T}(a.val*real(y.val), a.val*imag(y.val)))
+    end
+
+    @inline function div_fast{T<:ComplexTypes}(x::Quantity{T}, y::Quantity{T})
+        D = typeof(dimension(x) / dimension(y))
+        U = typeof(unit(x) / unit(y))
+        Quantity{T,D,U}(T(real(x.val)*real(y.val) + imag(x.val)*imag(y.val),
+          imag(x.val)*real(y.val) - real(x.val)*imag(y.val))) / abs2(y)
+    end
+    function div_fast{T<:FloatTypes}(x::Quantity{Complex{T}}, b::Quantity{T})
+        D = typeof(dimension(x) / dimension(y))
+        U = typeof(unit(x) / unit(y))
+        Quantity{T,D,U}(Complex{T}(real(x.val)/b.val, imag(x.val)/b.val))
+    end
+    function div_fast{T<:FloatTypes}(a::Quantity{T}, y::Quantity{Complex{T}})
+        D = typeof(dimension(x) / dimension(y))
+        U = typeof(unit(x) / unit(y))
+        Quantity{T,D,U}(Complex{T}(a.val*real(y.val), -a.val*imag(y.val))) / abs2(y)
+    end
+    
+    eq_fast{T<:ComplexTypes}(x::Quantity{T}, y::Quantity{T}) =
+        (real(x)==real(y)) & (imag(x)==imag(y))
+    # eq_fast{T<:FloatTypes}(x::Complex{T}, b::T) = #TODO
+    #     (real(x)==b) & (imag(x)==T(0))
+    # eq_fast{T<:FloatTypes}(a::T, y::Complex{T}) =
+    #     (a==real(y)) & (T(0)==imag(y))
+
+    ne_fast{T<:ComplexTypes,D,U}(x::Quantity{T,D,U}, y::Quantity{T,D,U}) = !(x==y)
+end
 
 for op in (:+, :-, :*, :/, :(==), :!=, :<, :<=, :cmp, :mod, :rem)
     op_fast = fast_op[op]
     @eval begin
-        # type promotion
-        $op_fast(x::Number, y::Number, zs::Number...) =
-            $op_fast(promote(x,y,zs...)...)
-        # fall-back implementation that applies after promotion
-        $op_fast{T<:Number}(x::T,ys::T...) = $op(x,ys...)
+        # Fallback method for Quantitys after promotion.
+        $op_fast{T<:Number}(x::Quantity{T},ys::Quantity{T}...) = $op(x,ys...)
     end
 end
+
+# exponentiation is not type-stable for Quantities anyway
+pow_fast{T<:FloatTypes}(x::Quantity{T}, y::Integer) = x^y
