d4f7d47f2e9195fcd3dff90391c5b85b8a176e44
Andrew Keller <andrew.keller.09@gmail.com>
2018-10-17 15:04:56 -0700

    Change implementation, also implement +/-.

--- src/typemath.jl
+++ src/typemath.jl
@@ -1,26 +1,90 @@
+# Same as Base.unwrap_unionall, but I don't want to rely on unexported functions.
+function unwrap_unionall(@nospecialize(a))
+    while isa(a, UnionAll)
+        a = a.body
+    end
+    return a
+end
+
 for f in (:/, :*)
-    @eval function $f(::Type{S}, ::Type{T}) where {S<:Quantity, T<:Quantity}
-        A = typeof($f(oneunit(S), oneunit(T)))
-        return A
+    @eval @generated function ($f(::Type{Q1}, ::Type{Q2}) where
+            {Q1 <: Quantity, Q2 <: Quantity})
+        Q1u = unwrap_unionall(Q1)
+        Q2u = unwrap_unionall(Q2)
+        T1, T2 = Q1u.parameters[1], Q2u.parameters[1]
+        D1, D2 = Q1u.parameters[2], Q2u.parameters[2]
+        U1, U2 = Q1u.parameters[3], Q2u.parameters[3]
+        if T1 isa TypeVar || T2 isa TypeVar
+            return :($(Quantity))
+        else
+            A = typeof($f(oneunit(T1), oneunit(T2)))
+            if D1 isa TypeVar || D2 isa TypeVar
+                return :($(Quantity{A}))
+            else
+                B = typeof($f(D1(), D2()))
+                if U1 isa TypeVar || U2 isa TypeVar
+                    return :($(Quantity{A,B}))
+                else
+                    C = typeof($f(oneunit(Q1), oneunit(Q2)))
+                    return :($C)
+                end
+            end
+        end
     end
-    @eval function $f(::Type{Quantity{S}}, ::Type{T}) where {S, T<:Quantity}
-        A = typeof($f(oneunit(S), oneunit(numtype(T))))
-        return Quantity{A}
+
+    @eval $f(::Type{Q}, ::Type{T}) where {T<:Number, Q<:Quantity} = $f(Q, Quantity{T})
+    @eval $f(::Type{T}, ::Type{Q}) where {T<:Number, Q<:Quantity} = $f(Quantity{T}, Q)
+end
+
+for f in (:+, :-)
+    @eval @generated function ($f(::Type{Q1}, ::Type{Q2}) where
+            {Q1 <: Quantity, Q2 <: Quantity})
+        Q1u = unwrap_unionall(Q1)
+        Q2u = unwrap_unionall(Q2)
+        T1, T2 = Q1u.parameters[1], Q2u.parameters[1]
+        D1, D2 = Q1u.parameters[2], Q2u.parameters[2]
+        U1, U2 = Q1u.parameters[3], Q2u.parameters[3]
+        if T1 isa TypeVar || T2 isa TypeVar
+            return :($(Quantity))
+        else
+            # need float because in general unit conversions may have floating-point factors
+            A = typeof($f(oneunit(float(T1)), oneunit(float(T2))))
+            if D1 isa TypeVar || D2 isa TypeVar
+                return :($(Quantity{A}))
+            else
+                (D1 == D2) || return :(throw(DimensionError($(D1()), $(D2()))))
+                if U1 isa TypeVar || U2 isa TypeVar
+                    return :($(Quantity{A,D1}))
+                else
+                    C = typeof($f(oneunit(Q1), oneunit(Q2)))
+                    return :($C)
+                end
+            end
+        end
     end
-    @eval function $f(::Type{T}, ::Type{Quantity{S}}) where {S, T<:Quantity}
-        A = typeof($f(oneunit(numtype(T)), oneunit(S)))
-        return Quantity{A}
+
+    @eval @generated function $f(::Type{N}, ::Type{Q}) where {N<:Number, Q<:Quantity}
+        Qu = unwrap_unionall(Q)
+        T = Qu.parameters[1]
+        D = Qu.parameters[2]
+        U = Qu.parameters[3]
+        if T isa TypeVar
+            return :($(Quantity))
+        else
+            A = typeof($f(oneunit(float(N)), oneunit(float(T))))
+            if D isa TypeVar
+                return :($(Quantity{A}))
+            else
+                (D <: typeof(NoDims)) || return :(throw(DimensionError($(D()), NoDims)))
+                if U isa TypeVar
+                    return :($(Quantity{A,D}))
+                else
+                    C = typeof($f(oneunit(N), oneunit(Q)))
+                    return :($C)
+                end
+            end
+        end
     end
-    @eval function ($f(::Type{Quantity{T1,D1}}, ::Type{Quantity{T2,D2,U2}})
-            where {T1,T2,D1,D2,U2})
-        A = typeof($f(oneunit(T1), oneunit(T2)))
-        B = typeof($f(D1(), D2()))
-        return Quantity{A, B}
-    end
-    @eval function ($f(::Type{Quantity{T1,D1,U1}}, ::Type{Quantity{T2,D2}})
-            where {T1,T2,D1,D2,U1})
-        A = typeof($f(oneunit(T1), oneunit(T2)))
-        B = typeof($f(D1(), D2()))
-        return Quantity{A, B}
-    end
+
+    @eval $f(::Type{Q}, ::Type{N}) where {N<:Number, Q<:Quantity} = $f(N, Q)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -559,18 +559,44 @@ end
         @test_throws ErrorException isapprox(1.0*FixedUnits(u"m"), 1000.0*FixedUnits(u"mm"))
     end
     @testset "> Type mathematics" begin
-        @test @inferred(typeof(1kg)*typeof(1s)) <: typeof(1kg*s)
-        @test @inferred(typeof(1kg)/typeof(1s)) <: typeof(1.0kg/s)
+        # concrete types
+        @test @inferred(typeof(1kg)+typeof(1kg)) <: typeof(1kg)
+        @test_throws Unitful.DimensionError (typeof(1kg)+typeof(1s))
+        @test @inferred(typeof(1kg)-typeof(1kg)) <: typeof(1kg)
+        @test_throws Unitful.DimensionError (typeof(1kg)-typeof(1s))
+        @test @inferred(typeof(1kg)*typeof(1s))  <: typeof(1kg*s)
+        @test @inferred(typeof(1kg)/typeof(1s))  <: typeof(1.0kg/s)
+
+        # unionalls
         let QFloat64 = Unitful.Quantity{Float64,D,U} where U where D,
-            QMFloat64 = Unitful.Quantity{Float64,typeof(𝐌),U} where U,
-            QMTFloat64 = Unitful.Quantity{Float64,typeof(𝐌*𝐓),U} where U,
-            QMbyTFloat64 = Unitful.Quantity{Float64,typeof(𝐌/𝐓),U} where U
+                QMFloat64 = Unitful.Quantity{Float64,typeof(𝐌),U} where U,
+                QMTFloat64 = Unitful.Quantity{Float64,typeof(𝐌*𝐓),U} where U,
+                QMbyTFloat64 = Unitful.Quantity{Float64,typeof(𝐌/𝐓),U} where U,
+                QNoDimsFloat64 = Unitful.Quantity{Float64,typeof(NoDims),U} where U
+            # mul, div
             @test @inferred(QFloat64  * typeof(1s)) <: QFloat64
             @test @inferred(QFloat64  / typeof(1s)) <: QFloat64
             @test @inferred(QMFloat64 * typeof(1s)) <: QMTFloat64
             @test @inferred(QMFloat64 / typeof(1s)) <: QMbyTFloat64
-        end
+            @test @inferred(QFloat64  * QMFloat64)  <: QFloat64
+            @test @inferred(QMFloat64 / QFloat64)   <: QFloat64
+            @test @inferred(Float64   * QFloat64)   <: QFloat64
+            @test @inferred(QFloat64  * Float64)    <: QFloat64
+            @test @inferred(Float64   / QFloat64)   <: QFloat64
+            @test @inferred(QFloat64  / Float64)    <: QFloat64
 
+            # add, sub
+            @test @inferred(QFloat64 + typeof(1s))  <: QFloat64
+            @test @inferred(QFloat64 - typeof(1s))  <: QFloat64
+            @test_throws Unitful.DimensionError (QMFloat64 + typeof(1s))
+            @test_throws Unitful.DimensionError (QMFloat64 - typeof(1s))
+            @test @inferred(Float64  + QFloat64)    <: QFloat64
+            @test @inferred(QFloat64 + Float64)     <: QFloat64
+            @test @inferred(Float64  - QFloat64)    <: QFloat64
+            @test @inferred(QFloat64 - Float64)     <: QFloat64
+            @test @inferred(QNoDimsFloat64 + Float64) <: QNoDimsFloat64
+            @test @inferred(typeof(1rad) - Float64) <: Float64
+        end
     end
 end
 
