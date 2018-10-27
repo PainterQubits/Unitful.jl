6c2c01b2b97d126557cf2940445cde59eb05b4bd
Andrew Keller <andrew.keller.09@gmail.com>
2018-10-27 15:13:53 -0700

    Try to coax doc builds to work, round 2.

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, Unitful
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs==0.16.3", "mkdocs-material", "python-markdown-math"),
+    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs==0.17.5", "mkdocs-material==2.9.4", "python-markdown-math"),
     julia  = "1.0",
     osname = "linux",
     repo   = "github.com/ajkeller34/Unitful.jl.git"
