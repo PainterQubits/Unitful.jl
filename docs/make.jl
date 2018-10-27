ced0f1111b12c9259d1b497c0f22f58837425143
Andrew Keller <andrew.keller.09@gmail.com>
2018-10-27 14:55:57 -0700

    Further docs updates.

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, Unitful
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs==0.16.3", "mkdocs-material", "python-markdown-math"),
     julia  = "1.0",
     osname = "linux",
     repo   = "github.com/ajkeller34/Unitful.jl.git"
