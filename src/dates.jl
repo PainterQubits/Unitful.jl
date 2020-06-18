c00045a10de7cd3a00fd05d0a54f5fc5fe051ff1
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2020-06-18 17:44:26 +0200

    Add Quantity(::FixedPeriod) docstring

--- src/dates.jl
+++ src/dates.jl
@@ -38,6 +38,21 @@ quantitytype(::Type{T}) where {T<:Dates.FixedPeriod} =
 
 ustrip(p::Dates.FixedPeriod) = Dates.value(p)
 
+"""
+    Quantity(period::Dates.FixedPeriod)
+
+Create a `Quantity` that corresponds to the given `period`. The numerical value of the
+resulting `Quantity` is of type `Int64`.
+
+# Example
+
+```jldoctest
+julia> using Dates: Second
+
+julia> Quantity(Second(5))
+5 s
+```
+"""
 Quantity(period::Dates.FixedPeriod) = Quantity(ustrip(period), unit(period))
 
 uconvert(u::Units, period::Dates.FixedPeriod) = uconvert(u, Quantity(period))
