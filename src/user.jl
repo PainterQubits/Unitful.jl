174eaca537144e7766c848d842358db830d0f730
Luke Bemish <lukebemish@gmail.com>
2021-08-21 23:14:13 -0600

    Warn user when preferunits causes redundant units
    Issue a warning when preferunits creates redundant units, which could stop
    it from sucessfully simplifying certain quantities.

--- src/user.jl
+++ src/user.jl
@@ -313,6 +313,25 @@ function preferunits(u0::Units, u::Units...)
         promotion[name(y)] = unit
     end
 
+    # Let's run a quick check for anything where, for instance, current was defined is C/ms and time
+    # was defined in ns. We'll give a warning if this is the case so the user knows they may get
+    # unexpected (though still correct) results. There may be cases where people would want to do this,
+    # so we won't stop them, just let them know they're doing it.
+    ulist = [([typeof(i[2]).parameters[1] for i in promotion]...)...]
+    ulist = [i^(1/i.power) for i in ulist]
+    check = Dict()
+    for i in ulist
+        k = string(dimension(i))
+        if haskey(check, k)
+            if i!=check[k]
+                @warn "Prefered units contain complex units based on units of the same dimension but different scales: "*string(i)*" and "*string(check[k])*
+                "\nThis may be intentional, but otherwise could lead to redundant units, such as ms/s or kg/g"
+            end
+        else
+            check[k] = i
+        end
+    end
+
     nothing
 end
 
