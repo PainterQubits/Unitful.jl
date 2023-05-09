12a877571ccc4480b7c5b8256fb56fb51eebfd4c
Alexander Plavin <alexander@plav.in>
2023-05-10 00:12:58 +0300

    Update ext/InverseFunctionsUnitfulExt.jl
    Co-authored-by: Sebastian Stock <42280794+sostock@users.noreply.github.com>

--- ext/InverseFunctionsUnitfulExt.jl
+++ ext/InverseFunctionsUnitfulExt.jl
@@ -3,6 +3,6 @@ using Unitful
 import InverseFunctions: inverse
 
 # `true` plays the role of 1, but doesn't promote unnecessary
-inverse(f::Base.Fix1{typeof(ustrip)}) = Base.Fix1(*, true*f.x)
+inverse(f::Base.Fix1{typeof(ustrip), <:Unitful.Units}) = Base.Fix1(*, true*f.x)
 
 end
