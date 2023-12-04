6d28e4cc3cef1d74135b66f2659c5872da7262f1
Romeo Valentin <romeov@stanford.edu>
2023-12-03 22:54:45 -0800

    Include one more example in docstring.

--- src/utils.jl
+++ src/utils.jl
@@ -253,11 +253,16 @@ Useful to build unitful interfaces that don't constrain the numeric type or the 
 Examples:
 
 ```jldoctest
-julia> circumference_of_square(side::WithDims(u"m")) = 4*side;
+julia> using Unitful, Unitful.DefaultSymbols; import Unitful.hr
+julia> circumference_of_square(side::WithDims(m)) = 4*side;
 julia> circumference_of_square((1//2)m)  # works
 2//1 m
 julia> circumference_of_square((1//2)km)  # also works
 2//1 km
+# You can also constrain the return type. The numeric type is usually inferred automatically.
+julia> kinetic_energy(mass::WithDims(kg), velocity::WithDims(m/s))::WithDims(J) = mass*velocity^2;
+julia> kinetic_energy(1000kg, 100km/hr)
+10000000 kg km^2 hr^-2
 ```
 
 See also [`Unitful.WithUnits`](@ref).
@@ -275,10 +280,16 @@ Useful to build unitful interfaces that don't constrain the unit, but not the nu
 Examples:
 
 ```jldoctest
-julia> circumference_of_square(side::WithUnits(u"m")) = 4*side;
+julia> using Unitful, Unitful.DefaultSymbols; import Unitful.hr
+julia> circumference_of_square(side::WithUnits(m)) = 4*side;
 julia> circumference_of_square((1//2)m)  # works
 2//1 m
 julia> # circumference_of_square((1//2)km)  # doesn't work, constrained to exactly meters
+
+# You can also constrain the return type. The numeric type is usually inferred automatically.
+julia> kinetic_energy(mass::WithUnits(kg), velocity::WithUnits(m/s))::WithUnits(J) = mass*velocity^2 |> x->uconvert(J, x)
+julia> kinetic_energy(1000kg, uconvert(m/s, 100km/hr))
+62500000//81 J
 ```
 
 See also [`Unitful.WithDims`](@ref).
