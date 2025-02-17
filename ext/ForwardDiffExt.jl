81650bcec69a4544e59668c445042b29bb182bc2
Isaac Wheeler <ickaser@gmail.com>
2025-02-17 20:09:35 +0100

    Dispatch on NoDims

--- ext/ForwardDiffExt.jl
+++ ext/ForwardDiffExt.jl
@@ -2,15 +2,8 @@ module ForwardDiffExt
 using Unitful
 using ForwardDiff
 
-function Base.convert(d::Type{ForwardDiff.Dual{T, V, N}}, q::Quantity) where {T, V, N}
-    if dimension(q) == NoDims
-        return d(uconvert(NoUnits, q))
-    else
-        throw(DimensionError(NoUnits,x))
-    end
+function Base.convert(d::Type{ForwardDiff.Dual{T, V, N}}, q::Quantity{T2, NoDims,U}) where {T, V, N, T2, U}
+    return d(uconvert(NoUnits, q))
 end
-# function convert(d::Type{ForwardDiff.Dual{T, V, N}}, q::Quantity{T,NoDims,U}) where {T, V, N, U}
-#     return ForwardDiff.Dual{T, V, N}(uconvert(NoUnits, q))
-# end
 
 end
