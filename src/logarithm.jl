14360f09af2e10d8accc25cc2f16867995917788
Chris Garling <chris.t.garling@gmail.com>
2025-05-19 19:47:36 -0400

    Implement sostock's feedback

--- src/logarithm.jl
+++ src/logarithm.jl
@@ -12,8 +12,8 @@ big(x::Level{L,S}) where {L,S} = Level{L,S}(big(x.val))
 """
     logunit(x::LogScaled)
     logunit(x::Union{Type{<:LogScaled}, MixedUnits})
-Returns the units associated with a `LogScaled` instance, a `LogScaled` type,
-or a `MixedUnits`.
+Returns the logarithmic "units" associated with a `LogScaled` instance, a 
+`LogScaled` type, or a `MixedUnits`.
 
 Examples:
 ```jldoctest
@@ -24,7 +24,12 @@ dB
 
 julia> logunit(u"dB")
 dB
+
+julia> logunit(u"dB/s")
+dB
 ```
+
+See also: [`unit`](@ref).
 """
 function logunit end
 logunit(x::Level{L,S}) where {L,S} = MixedUnits{Level{L,S}}()
