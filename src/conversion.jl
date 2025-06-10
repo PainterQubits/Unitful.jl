e5aceb5360cc572f7f750f9d5f4d90b5e2f49405
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2025-06-10 18:52:36 +0200

    Rename floattype → convfact_floattype

--- src/conversion.jl
+++ src/conversion.jl
@@ -53,7 +53,7 @@ Returns the appropriate conversion factor from unit `t` to unit `s` for the numb
 function convfact(::Type{T}, s::Units, t::Units) where T
     cf = convfact(s, t)
     if cf isa AbstractFloat
-        F = floattype(T)
+        F = convfact_floattype(T)
         # Since conversion factors only have Float64 precision,
         # there is no point in converting to BigFloat
         convert(F == BigFloat ? Float64 : F, cf)
@@ -62,19 +62,19 @@ function convfact(::Type{T}, s::Units, t::Units) where T
     end
 end
 
-function floattype(::Type{T}) where T
+function convfact_floattype(::Type{T}) where T
     # Use try-catch instead of hasmethod because a
     # fallback method might exist but throw an error
     try
-        _floattype(float(real(T)))
+        _convfact_floattype(float(real(T)))
     catch
         Float64
     end
 end
 
-_floattype(::Type) = Float64
-_floattype(::Type{Float16}) = Float16
-_floattype(::Type{Float32}) = Float32
+_convfact_floattype(::Type) = Float64
+_convfact_floattype(::Type{Float16}) = Float16
+_convfact_floattype(::Type{Float32}) = Float32
 
 """
     uconvert(a::Units, x::Quantity{T,D,U}) where {T,D,U}
